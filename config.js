41bc27e636dda0e48439509062845d50590b3396
beccablake <theconfusededone@gmail.com>
2026-05-29 20:14:24 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Edit this file once with your credentials.
 // Never need to touch it again unless your token changes.
 
-const AIRTABLE_READ_TOKEN = "patZFLL9my9M5g7YD.2e3ef22a1c5b2782dbffebab1cff1a587404e2eadbf0cab5cf9e25d7d4188070";
+const AIRTABLE_READ_TOKEN = "patZFLL9my9M5g7YD.49be93bb8c19cb7c6653bd59fe5f8208e1aa50634f7c9ffc187c1981b074839b";
 const AIRTABLE_BASE_ID    = "appMvqC0aFTB1XgkM";
 const AIRTABLE_TABLE      = "Table 1"; // Update if you rename your table in Airtable
