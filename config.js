8082c6550e531947edd3ff5b27ee6722ec62066a
beccablake <theconfusededone@gmail.com>
2026-05-30 21:54:38 -0700

    Update config.js
    added write token (same as read token)

--- config.js
+++ config.js
@@ -5,3 +5,4 @@
 const AIRTABLE_READ_TOKEN = "patZFLL9my9M5g7YD.49be93bb8c19cb7c6653bd59fe5f8208e1aa50634f7c9ffc187c1981b074839b";
 const AIRTABLE_BASE_ID    = "appMvqCOaFTB1XgkM";
 const AIRTABLE_TABLE      = "Listings"; // Update if you rename your table in Airtable
+const AIRTABLE_WRITE_TOKEN = "patZFLL9my9M5g7YD.49be93bb8c19cb7c6653bd59fe5f8208e1aa50634f7c9ffc187c1981b074839b";
